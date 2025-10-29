45db2b015271490720fff2c7344a9c3338d31bd2
jj-attaq <jj.attaq@gmail.com>
2025-10-29 12:52:05 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port must be set")
+	}
+	fmt.Println(port)
+}
