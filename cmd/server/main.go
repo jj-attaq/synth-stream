c9195c87dacc75a38ce9e204af1c3af7f12a3aeb
jj-attaq <jj.attaq@gmail.com>
2026-02-04 09:24:09 +0100

    reorganized

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,30 +2,139 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"os"
+	"net"
+	"strings"
+	"sync"
 
-	"github.com/joho/godotenv"
+	"github.com/google/uuid"
 )
 
-// NOTES
-// For auth, this is a good library: https://github.com/markbates/goth
+type session struct {
+	ID           string
+	Participants []string
+	// Events       []midiEvent
+	Listener net.Listener
+	mu       sync.RWMutex
+}
+
+func (s *session) addParticipant(p string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.Participants = append(s.Participants, p)
+	fmt.Printf("Number of participants: %d\n", len(s.Participants))
+}
+
+// var users = []string{"A", "B"}
+// var eventTypes = []string{"note_on", "note_off"}
+//
+// type midiEvent struct {
+// 	Type string
+// 	User string
+// 	Note int
+// }
+//
+// func newMidiEvent(t, u string, n int) (midiEvent, error) {
+// 	if !slices.Contains(eventTypes, t) {
+// 		return midiEvent{}, fmt.Errorf("Not a valid event type")
+// 	}
+//
+// 	if !slices.Contains(users, u) {
+// 		return midiEvent{}, fmt.Errorf("Not a valid user")
+// 	}
+//
+// 	return midiEvent{
+// 		Type: t,
+// 		User: u,
+// 		Note: n,
+// 	}, nil
+// }
+
+func newSession(addr string) (session, error) {
+	newUUID := uuid.New()
+	id := newUUID.String()
+
+	ln, err := net.Listen("tcp", ":"+addr)
+	if err != nil {
+		panic(err)
+	}
+
+	return session{
+		ID:       id,
+		Listener: ln,
+	}, nil
+}
 
 func main() {
-	godotenv.Load()
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("Port must be set")
+	// TCP Server
+	s, err := newSession("8080")
+	if err != nil {
+		panic(err)
 	}
 
-	mux := http.NewServeMux()
+	for {
+		conn, err := s.Listener.Accept()
+		if err != nil {
+			panic(err)
+		}
 
-	srv := http.Server{
-		Addr:    fmt.Sprintf(":%s", port),
-		Handler: mux,
+		go handleConnection(conn, &s)
 	}
-
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
 }
+
+// echo tcp
+func handleConnection(conn net.Conn, s *session) {
+	defer conn.Close()
+
+	// username or identifier
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+
+	// parse username
+	userName := string(buffer[:n])
+	userName = strings.TrimSpace(userName)
+
+	// add
+	s.addParticipant(userName)
+
+	// echo back
+	_, err = conn.Write(buffer[:n])
+	if err != nil {
+		fmt.Println("Error writing:", err)
+	}
+}
+
+// package main
+//
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+//
+// 	"github.com/joho/godotenv"
+// )
+//
+// // NOTES
+// // For auth, this is a good library: https://github.com/markbates/goth
+//
+// func main() {
+// 	godotenv.Load()
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		log.Fatal("Port must be set")
+// 	}
+//
+// 	mux := http.NewServeMux()
+//
+// 	srv := http.Server{
+// 		Addr:    fmt.Sprintf(":%s", port),
+// 		Handler: mux,
+// 	}
+//
+// 	log.Printf("Serving on port: %s\n", port)
+// 	log.Fatal(srv.ListenAndServe())
+// }
