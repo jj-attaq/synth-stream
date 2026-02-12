8985386a71ff275470cae8f70378cddfb5e698a4
jj-attaq <jj.attaq@gmail.com>
2026-02-12 14:25:36 +0100

    started protocol design

--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -1,9 +1,22 @@
 package protocol
 
 import (
-	"io"
+	"encoding/binary"
+	"errors"
 	"net"
 )
+
+const (
+	TypeText  = 0x01
+	TypeMidi  = 0x10
+	TypeAudio = 0x20
+)
+
+var validTypes = map[byte]struct{}{
+	TypeText:  {},
+	TypeMidi:  {},
+	TypeAudio: {},
+}
 
 type Packet struct {
 	Header
@@ -11,15 +24,26 @@ type Packet struct {
 }
 
 type Header struct {
-	Type   [1]byte
-	Length [2]byte
+	Type   byte
+	Length uint16
 }
 
-func WriteMessage(conn net.Conn, msgType byte, payload []byte) {
-	io.
+func (h Header) Marshal() ([]byte, error) {
+	buf := make([]byte, 3)
+	if _, ok := validTypes[h.Type]; !ok {
+		return nil, errors.New("invalid type")
+	}
+
+	buf[0] = h.Type
+	binary.BigEndian.PutUint16(buf[1:], h.Length)
+
+	return buf, nil
 }
 
-// func WriteMessage(conn net.Conn, msgType) {
-// 	buf := append([]byte, header.Type, header.Length)
-// 	io.ReadFull(conn, buf)
-// }
+func (h Header) Unmarshal(data []byte, v any) error
+
+func (h Header) Read(p []byte) (n int, err error)
+
+func (h Header) Write(p []byte) (n int, err error)
+
+func WriteMessage(conn net.Conn, msgType byte, payload []byte)
